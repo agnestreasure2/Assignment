064e38648861b4e14b941460c5c2aba03da4ab54
Agnes <agnesagbo6@gmail.com>
2023-05-01 00:37:42 -0700

    first assignment

---
+++ src/components/HeaderContent.js
@@ -0,0 +1,119 @@
+import { View, Text, Image, TextInput, Platform } from "react-native";
+import React, { useState } from "react";
+
+const HeaderContent = () => {
+  const [search, setSearch] = useState("");
+
+  return (
+    <View
+      style={{
+        flex: Platform.OS === "ios" ? 0.6 : 0.5,
+      }}
+    >
+      {/* menu icons */}
+      <View
+        style={{
+          marginTop: Platform.OS === "ios" ? 60 : 35,
+          marginHorizontal: 17,
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Image
+            source={require("../assets/icons/home.png")}
+            style={{ width: 25, height: 25, tintColor: "white" }}
+          />
+        </View>
+        <View style={{ paddingRight: 25, justifyContent: "center" }}>
+          <Image
+            source={require("../assets/icons/home.png")}
+            style={{ width: 25, height: 25, tintColor: "white" }}
+          />
+        </View>
+        <View style={{ justifyContent: "center" }}>
+          <Image
+            source={require("../assets/icons/home.png")}
+            style={{ width: 25, height: 25, tintColor: "white" }}
+          />
+        </View>
+      </View>
+
+      {/* header text */}
+      <View style={{ marginTop: 30, marginLeft: 16 }}>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 25,
+            fontWeight: "600",
+            lineHeight: 30,
+          }}
+        >
+          Delicious food ready to be
+        </Text>
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <View style={{ justifyContent: "center" }}>
+            <Text
+              style={{
+                color: "white",
+                fontSize: 25,
+                fontWeight: "600",
+                lineHeight: 30,
+              }}
+            >
+              delivered for you
+            </Text>
+          </View>
+          <View style={{ flex: 1, paddingLeft: 10, justifyContent: "center" }}>
+            <Image
+              source={require("../assets/icons/home.png")}
+              style={{ width: 25, height: 25, tintColor: "red" }}
+            />
+          </View>
+        </View>
+      </View>
+
+      {/* search bar */}
+      <View
+        style={{
+          backgroundColor: null,
+          height: 50,
+          borderWidth: 0.5,
+          borderColor: "white",
+          marginTop: 20,
+          flexDirection: "row",
+          paddingHorizontal: 15,
+          marginHorizontal: 30,
+          borderRadius: 12,
+        }}
+      >
+        <View style={{ justifyContent: "center" }}>
+          <Image
+            source={require("../assets/icons/search.png")}
+            style={{
+              width: 20,
+              height: 20,
+              justifyContent: "center",
+              tintColor: "white",
+            }}
+          />
+        </View>
+
+        <TextInput
+          autoFocus={false}
+          onChangeText={setSearch}
+          value={search}
+          placeholder={"Search something..."}
+          placeholderTextColor={"white"}
+          style={{
+            color: "black",
+            paddingLeft: 12,
+            justifyContent: "center",
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default HeaderContent;
---
+++ src/external_data/externalData.js
@@ -0,0 +1,33 @@
+const categories = [
+    {
+      id: 0,
+      title: "Pizza",
+      image: require("../assets/images/food4.jpg"),
+      price: 20,
+      rating: 2
+    },
+    {
+      id: 1,
+      title: "Small Chops",
+      image: require("../assets/images/food3.jpg"),
+      price: 25,
+      rating: 3.7
+    },
+    {
+      id: 2,
+      title: "Fries",
+      image: require("../assets/images/food2.jpg"),
+      price: 15,
+      rating: 4
+    },
+    {
+      id: 3,
+      title: "Vegetables",
+      image: require("../assets/images/food1.jpg"),
+      price: 30,
+      rating: 5
+    },
+  ];
+  
+  export { categories };
+  
